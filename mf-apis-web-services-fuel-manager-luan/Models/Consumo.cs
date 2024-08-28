7aeba180384219f91541a47c419d0369a5313121
hashtagluan <128554958+hashtagluan@users.noreply.github.com>
2024-08-28 19:01:59 -0300

    criação os models

---
+++ mf-apis-web-services-fuel-manager-luan/Models/Consumo.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_manager_luan.Models
+{
+    [Table("Consumos")]
+    public class Consumo
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public decimal Valor { get; set; }
+        [Required]
+        public TipoCombustivel Tipo { get; set; }
+
+        [Required]
+        public int VeiculoId { get; set; }
+
+        public Veiculo Veiculo { get; set; }
+
+    }
+
+    public enum TipoCombustivel
+    {
+
+        Diesel,
+        Etanol,
+        Gasolina
+
+    }
+}
