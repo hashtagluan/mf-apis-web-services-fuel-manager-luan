0fef1132b123f48adc9b8ce4da9abcf7114e649d
hashtagluan <128554958+hashtagluan@users.noreply.github.com>
2024-08-30 15:00:49 -0300

    configuração do Enity Framework

---
+++ mf-apis-web-services-fuel-manager-luan/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mf_apis_web_services_fuel_manager_luan.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Consumo> Consumos { get; set; }
+
+    }
+}
